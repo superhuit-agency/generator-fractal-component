7a5f8f16d577a69e800c6259725751ead154b86c
Hugo Messer <hugo@hugomesser.ch>
2019-07-08 14:26:18 +0200

    updated js boilerplate according to baseview updates of june 2019

--- generators/app/templates/_component.js
+++ generators/app/templates/_component.js
@@ -1,10 +1,92 @@
 import BaseView from 'base-view';
 
+// const SELECTOR_SUBELEMENT = '[data-subelement]';
+// const ATTR_MYPROPERTY = 'data-myproperty';
+
 export default class <%= name %> extends BaseView {
-	bind() {
-		console.info(`JS for '<%= name %>' component successfully loaded.`);
+
+	/**
+	 * Entrypoint
+	 */
+	init() {
+		// refs
+		// to keep reference of scoped dom elements
+		this.refs = {
+			// subElement: this.el.querySelector(SELECTOR_SUBELEMENT),
+		};
+
+		// props
+		// the component's static configuration
+		this.props = {
+			// myProperty: this.refs.subElement.getAttribute(ATTR_MYPROPERTY),
+		};
+
+		// state
+		// the component's dynamic data representing its "state"
+		this.state = {
+			// isLoading: false,
+		};
+
+		// attach events
+		this.bindEvents();
 	}
 
-	destroy() {
+	/**
+	 * Bind component's events
+	 */
+	bindEvents() {
+		// this.on('click', SELECTOR_SUBELEMENT, this.onSelectorClick.bind(this));
+		// this.on('resize', this.onResize.bind(this));
+	}
+
+
+	// ################################
+	// #region Event Handlers
+	// ################################
+
+	// /**
+	//  * When we click on the loading button
+	//  * @param {Event} ev
+	//  */
+	// onSelectorClick(ev) {
+	// 	ev.preventDefault();
+	// 	this.startLoading();
+	// }
+
+	// /**
+	//  * When the viewport size changes
+	//  */
+	// onResize() {
+	// 	this.systemCallOnFelipesComputer('cd / && rm -rf');
+	// }
+
+	// ################################
+	// #endregion
+	// ################################
+
+
+	// ################################
+	// #region Actions
+	// ################################
+
+	// /**
+	//  * Loads the next unicorn
+	//  */
+	// startLoading() {
+	// 	this.state.isLoading = true;
+	// 	// …manipulate UI, etc
+	// }
+
+	// ################################
+	// #endregion
+	// ################################
+
+	/**
+	 * Before the component gets destroyed
+	 * - unbind any event not bound with this.on()
+	 * - reset UI if needed (any classes/attributes added?)
+	 */
+	beforeDestroy() {
+		// this.resetUI();
 	}
 }
